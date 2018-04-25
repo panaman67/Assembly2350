59cf483f4c93fd586f053bcf4ef2632805f6e4d5
Nick Paladino <nickp@ncpholdings.org>
2018-04-25 01:21:20 -0500

    Add files via upload

---
+++ fact.asm
@@ -0,0 +1,57 @@
+extern printf
+extern scanf
+
+SECTION .data
+
+	msg:  db "Hello! %d", 10, 0
+
+	menu: db "0: combonation", 10, "1: permutation", 0
+
+	sfmt: db "%c", 0
+
+	ans:  dq 4
+
+SECTION .text
+
+; THIS FUCKING WORKS!!!!!!!
+; Prints: Hello! <value returned by fact>
+global main
+main:
+	mov  rdi, menu
+	mov  rax, 0
+	call printf
+
+	;mov  rdi, sfmt
+	;mov  rsi, ans
+
+	push DWORD 6
+
+	call fact
+	mov  rsi, rax
+	mov  rax, 0
+	call printf
+	mov  eax, 1    ; 1 is code for exit
+	int  80h
+
+; Returns factorial of pushed num
+fact:
+	push rbp
+	mov  rbp, rsp
+
+	mov  rax, [rbp + 16]
+	cmp  rax, 1
+	je   end
+
+	dec  rax
+	push rax
+	call fact
+	
+	mov  rbx, [rbp + 16]
+	mul  rbx
+
+  end:
+	mov  rsp, rbp
+	pop  rbp
+	ret
+	
+	
