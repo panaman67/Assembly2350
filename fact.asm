cb59bd43b316e5ec5adc374ad8c82050cf071101
Nick Paladino <nickp@ncpholdings.org>
2018-04-26 23:36:55 -0500

    Lots of changes, made the factorial loop work

--- fact.asm
+++ fact.asm
@@ -3,40 +3,57 @@ extern scanf
 
 SECTION .data
 
-	msg:  db "Hello! %d", 10, 0
+	msg:       db "Factorial: %ld", 10, 0
 
-	menu: db "0: combonation", 10, "1: permutation", 10, 0
+	prompt:    db "What num to be factorialized: ", 0
 
-	sfmt: db "%c", 0
+	sfmt:      db "%d", 0
 
-	ans:  dq 0
+	menu:      db "0: combonation", 10, "1: permutation", 10, 0
+
+	ans:       dq 0
+
+	playagain: db "Play again: ", 0
+
+	again:     db 'n', 0
+
+	yes:       db 'y', 0
 
 SECTION .text
 
 ; THIS FUCKING WORKS!!!!!!!
-; Prints: Hello! <value returned by fact>
 global main
 main:
-	mov  rdi, menu
-	mov  rax, 0
-	call printf
-	
-	;lea    rsi,[rbp-0x1]
-	;mov    BYTE PTR [rbp-0x1],0x20
-	;mov    al,0x0
-	;call   400490 <__isoc99_scanf@plt>
-	;movabs rdi,0x400657
-	lea  rsi, [ans]
-	mov  QWORD [ans], 0x20
-	mov  rax, 0
-	call scanf
 
-	push QWORD [ans]
+   menustart:             ; Label for start of loop
+	mov  rdi, prompt  ; move the prompt to RDI (1st parameter loc)
+	;mov  rsi, 0
+	mov  rax, 0       ; needed to disable MMX instructions ?????
+	call printf       ; call extern printf proc
+	; printf("Enter num: ");
 	
-	call fact
+	mov  rdi, sfmt    ; move input format to rdi
+	mov  rsi, ans     ; move location of ans var to rsi
+	mov  rax, 0       ; needed to disable MMX????
+	call scanf        ; call scanf
+	; scanf("");
+
+	push QWORD [ans]  ; push contents of ans to stack
+	call fact         ; call factorial function
+
+	mov  rdi, msg     ; printf stuff for factorial answer
 	mov  rsi, rax
 	mov  rax, 0
 	call printf
+
+	mov  rdi, playagain ; prompt to ask to repeat
+	mov  rsi, again
+	mov  rax, 0
+	call scanf
+
+	; FIXME	
+	cmp  BYTE [again], 'y'   ; for checking if we should loop back
+	jne  menustart
 	mov  eax, 1    ; 1 is code for exit
 	int  80h
 
@@ -47,7 +64,7 @@ fact:
 
 	mov  rax, [rbp + 16]
 	cmp  rax, 1
-	je   end
+	jle  end
 
 	dec  rax
 	push rax
@@ -56,7 +73,7 @@ fact:
 	mov  rbx, [rbp + 16]
 	mul  rbx
 
-  end:
+     end:
 	mov  rsp, rbp
 	pop  rbp
 	ret
