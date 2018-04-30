4382bd62b835ce588d11fa32bb68133dc937c4f4
Nick Paladino <nickp@ncpholdings.org>
2018-04-30 10:44:04 -0500

    Added direction comments

--- fact.asm
+++ fact.asm
@@ -1,3 +1,15 @@
+;Create an assembly program that will calculate a user's choice of either combination orpermutation.
+; -Your main procedure will print a menu for the user and read their input.
+;      factorial, combination and permutation.
+; -Factorial should be a recursively defined procedure that takes an integer argument
+;      (passed on the stack) and returns it's factorial.
+; -The combination and permutation procedures will use the factorial procedure
+;      that you create in order to perform their calculations.
+; -After each selection the user should be asked if they would like to perform another operation.
+; -If they enter "yes" (not just 'y') they should be able to continue.
+; -Otherwise, the program should exit
+
+
 extern printf
 extern scanf
 
